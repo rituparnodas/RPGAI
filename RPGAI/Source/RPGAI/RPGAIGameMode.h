12b47c24ea0e6801cd7f497f1aca8324c8d70883
Dev Das <devdastour@gmail.com>
2022-01-19 18:33:57 +0530

    Added Project Files!!!

---
+++ RPGAI/Source/RPGAI/RPGAIGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RPGAIGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ARPGAIGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ARPGAIGameMode();
+};
+
+
+
